b9f9f76ee7a08eafd52b28c59eaad634d8758391
Kevin Wang <kwang30@stuy.edu>
2022-08-22 22:22:21 -0400

    init

---
+++ hardhat.config.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+require('hardhat-contract-sizer');
+require("hardhat-gas-reporter");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  mocha: {
+    timeout: 100000000
+  },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000000,
+      },
+    }
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_KEY,
+        blockNumber: 14667418 // 2022-04-27
+      }
+    },
+    polygon: {
+      url: "https://rpc.ankr.com/polygon",
+      chainId: 137
+    },
+    mainnet: {
+      url: "https://rpc.ankr.com/eth",
+      chainId: 1
+    },
+    optimism: {
+      url: "https://rpc.ankr.com/optimism",
+      chainId: 10
+    },
+    arbitrum: {
+      url: "https://rpc.ankr.com/arbitrum",
+      chainId: 42161
+    }
+  }
+};
